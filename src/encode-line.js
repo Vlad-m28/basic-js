1dbcea80463d75c5af5bb8d3a2edd0c9b1ee6425
Vlad <vlad.murylyv@mail.ru>
2025-01-05 20:30:50 +0300

    feat: finish 2 tests

--- src/encode-line.js
+++ src/encode-line.js
@@ -10,9 +10,18 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  let res = '';
+  let count = 1;
+  for (let i = 0; i < str.length; i++) {
+  let item = '';
+    if (str[i] !== str[i + 1]) {
+      item = `${count}${str[i]}`;
+      res += item;
+      count = 1;
+    } else count += 1;
+  }
+  return res.replaceAll('1', '');
 }
 
 module.exports = {
--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -15,9 +15,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  * => 'STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS**STRINGPLUS00PLUS00PLUS'
  *
  */
-function repeater(/* str, options */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function repeater(str, options) {
+  if (!options.separator) options.separator = '+';
+  if (!options.additionSeparator) options.additionSeparator = '|';
+  if (!options.repeatTimes) options.repeatTimes = 1;
+  if (!options.additionRepeatTimes) options.additionRepeatTimes = 1;
+
+  if (options.addition || typeof(options.addition) === 'boolean' || options.addition === null) {
+    let additionElem;
+    if (String(options.addition) === String(options.addition).trim()) {
+      additionElem = `${options.addition}     `.repeat(options.additionRepeatTimes).trimEnd().split('     ').join(options.additionSeparator);
+      return `${str}${additionElem}     `.repeat(options.repeatTimes).trimEnd().split('     ').join(options.separator);
+    } else {
+      additionElem = `${options.addition}^`.repeat(options.additionRepeatTimes).split('^').slice(0, -1).join(options.additionSeparator);
+      return `${str}${additionElem}^`.repeat(options.repeatTimes).split('^').slice(0, -1).join(options.separator);
+    }
+  }
+  return `${str} `.repeat(options.repeatTimes).trim().split(' ').join(options.separator);
 }
 
 module.exports = {
